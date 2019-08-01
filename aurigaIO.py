bda48da511a8be6e20885eb003e948e02a55e0d9
hyang <h.yang@mail.bnu.edu.cn>
2019-08-01 14:38:47 +0800

    test

--- aurigaIO.py
+++ aurigaIO.py
@@ -1,1 +1,11 @@
-import Auriga 
+from env import Auriga 
+import sys
+import matplotlib as plt
+
+basePATH = "/Simulations/Auriga/level4_DM/halo_6/output"
+dm_pos = Auriga.snapshot.loadSubset(basePATH,127,'dm',['Coordinates'])
+x = dm_pos[:,0]
+y = dm_pos[:,1]
+
+plt.plot
+print(type(dm_pos))
