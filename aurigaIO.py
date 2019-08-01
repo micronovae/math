985d6616711f545f4f672bdafeb93556f22178d7
Yang <h.yang@mail.bnu.edu.cn>
2019-08-01 15:44:02 +0800

    test

--- aurigaIO.py
+++ aurigaIO.py
@@ -9,8 +9,19 @@ pos = dm_pos['Coordinates']
 
 x = pos[:,0]
 y = pos[:,1]
+z = pos[:,2]
 
+plt.figure(0)
 plt.hist2d(x,y,norm=mpl.colors.LogNorm(),bins=64)
-plt.savefig(fname='tests.png')
+plt.savefig(fname='x-y.png')
+
+plt.figure(1)
+plt.hist2d(x,z,norm=mpl.colors.LogNorm(),bins=64)
+plt.savefig(fname='x-z.png')
+
+plt.figure(2)
+plt.hist2d(y,z,norm=mpl.colors.LogNorm(),bins=64)
+plt.savefig(fname='y-z.png')
+
 print(dm_pos['Coordinates'])
 print(type(dm_pos))
