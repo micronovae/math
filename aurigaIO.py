7ec2891da5fda3d1ac0f1d910e9e677cbc0a645f
Yang <h.yang@mail.bnu.edu.cn>
2019-08-01 15:36:18 +0800

    test

--- aurigaIO.py
+++ aurigaIO.py
@@ -1,11 +1,16 @@
 from env import Auriga 
 import sys
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 basePATH = "/Simulations/Auriga/level4_DM/halo_6/output"
 dm_pos = Auriga.snapshot.loadSubset(basePATH,127,'dm',['Coordinates'])
-x = dm_pos[:,0]
-y = dm_pos[:,1]
+pos = dm_pos['Coordinates']
 
-plt.plot
+x = pos[:,0]
+y = pos[:,1]
+
+plt.hist2d(x,y,norm=mpl.colors.LogNorm(),bins=64)
+plt.savefig(fname='tests.png')
+print(dm_pos['Coordinates'])
 print(type(dm_pos))
